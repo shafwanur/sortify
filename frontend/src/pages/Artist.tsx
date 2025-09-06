a78c9a320bc3233f8dd5da3cf96e6dcf1f781614
Shaf <shafwanur.kazi@gmail.com>
2025-09-06 15:13:03 +0200

    added sorting endpoint to frontend

--- frontend/src/pages/Artist.tsx
+++ frontend/src/pages/Artist.tsx
@@ -1,3 +1,4 @@
+import axios from "axios"
 import * as React from "react"
 import { Button } from "@/components/ui/button"
 import {
@@ -27,10 +28,57 @@ export default function CardDemo() {
   const location = useLocation();
   const {img, artistName, spotifyUri, followerCount} = location.state || {};
 
-  const [position, setPosition] = React.useState("Sort Type")
+  const [position, setPosition] = React.useState("all-of")
 
-  function handleClick() {
+  async function handleClick() {
     console.log(position);
+
+    const jwt_token = localStorage.getItem("jwt_token");
+    
+    const validate_response = await axios({
+      method: 'get',
+      url: 'http://localhost:8000/auth/validate',
+      headers: {
+        "Authorization": `Bearer ${jwt_token}`
+      }
+    });
+    
+    const spotify_user_id = validate_response.data.spotify_user_id;
+    const access_token_response = await axios.post('http://localhost:8000/spotify/token/access', {
+      spotify_user_id: spotify_user_id,
+      refresh_token: validate_response.data.refresh_token
+    })
+  
+    const access_token = access_token_response.data.access_token;
+    
+    // console.log(access_token);
+    console.log(spotifyUri.split(":").pop()!);
+
+    interface SortRequest {
+      artist_name: string;
+      artist_id: string;
+      arg: string;
+      spotify_user_id: string;
+    }
+
+
+    const payload: SortRequest = {
+      artist_name: artistName,
+      artist_id: spotifyUri.split(":").pop()!,
+      arg: position,
+      spotify_user_id: spotify_user_id,
+    };
+    const sort_response = await axios.post(
+      "http://localhost:8000/api/sort",
+      payload,
+      {
+        headers: {
+          "access-token": access_token, // raw header key
+        },
+      }
+    );
+
+    console.log(sort_response);
     // TODO: create backend endpoint to actually call this. 
   }
 
@@ -52,9 +100,9 @@ export default function CardDemo() {
                 <DropdownMenuLabel>Select Sort Type:</DropdownMenuLabel>
                 <DropdownMenuSeparator />
                 <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
-                  <DropdownMenuRadioItem value="All">All</DropdownMenuRadioItem>
-                  <DropdownMenuRadioItem value="Album">Album</DropdownMenuRadioItem>
-                  <DropdownMenuRadioItem value="Global">Global</DropdownMenuRadioItem>
+                  <DropdownMenuRadioItem value="all-of">all-of</DropdownMenuRadioItem>
+                  <DropdownMenuRadioItem value="album-sort">album-sort</DropdownMenuRadioItem>
+                  <DropdownMenuRadioItem value="global-sort">global-sort</DropdownMenuRadioItem>
                 </DropdownMenuRadioGroup>
               </DropdownMenuContent>
             </DropdownMenu>
