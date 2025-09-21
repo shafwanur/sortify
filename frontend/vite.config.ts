e3815df365f03422a3e8746d690a0a9003249804
Shaf <shafwanur.kazi@gmail.com>
2025-09-21 10:39:21 +0200

    added frontend endpoint as a valid host

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,29 @@
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+// Import 'loadEnv' from Vite
+import { defineConfig, loadEnv } from "vite"
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // The third parameter ('') loads all env vars without the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+    server: {
+      host: true, // Listen on all network interfaces
+      hmr: {
+        // Use the environment variable here
+        host: env.FRONTEND_ENDPOINT
+      },
+      // And also here
+      allowedHosts: [env.FRONTEND_ENDPOINT],
+    },
+  }
 })
